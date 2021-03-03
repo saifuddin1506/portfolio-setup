c25c2ba163cf123b652979502055aca4b5124c8c
saifuddin <mosaifuddeen200@gmail.com>
2021-03-04 02:36:06 +0530

    header added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
+import Particles from 'react-particles-js'
 import Navbar from './components/Navbar'
+import Header from './components/Header'
 import './App.css';
+import { faCircle } from '@fortawesome/free-solid-svg-icons'
 function App() {
   return (
     <div>
+      <Particles
+        className="particles-can"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 6,
+                color: "#f9ab00"
+              }
+            }
+          }
+        }}
+      />
       <Navbar />
+      <Header />
     </div>
 
   );
